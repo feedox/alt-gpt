a3c3378764381efa4594a217a90e26cf6e372313
Elya Livshitz <elya.livshitz@gmail.com>
2023-03-26 10:32:17 +0300

    * redirect main to chat * cleanup main

--- src/scripts/ts/app/app.routes.ts
+++ src/scripts/ts/app/app.routes.ts
@@ -4,10 +4,10 @@ import helpers from '/scripts/ts/app/app.helpers.js';
 import { router, baseRoutes, applyRoutes } from 'frame.libx.js/build-web/scripts/ts/app/app.routes.js';
 
 const routes = {
-	'/': { component: helpers.lazyLoader('/views/main.vue.js') },
+	'/': { component: helpers.lazyLoader('/views/main.vue.js'), redirect: '/chat' },
 
 	// views
-	'/chat': { component: helpers.lazyLoader('/views/chat.vue.js') }, /* , redirect: '/ai/bot/'  */
+	'/chat': { component: helpers.lazyLoader('/views/chat.vue.js') },
 	'/profile': { component: helpers.lazyLoader('/frame/views/misc/login.vue.js') },
 
 	// misc:
--- src/views/main.vue
+++ src/views/main.vue
@@ -1,93 +1,20 @@
 <template lang="pug">
 	.view.content.hero.is-fullheight-with-navbar.fg-white
-		img.sitebg(src="/resources/imgs/bg_matrix_optimized.gif")
-		// Hero head: will stick at the top
 		.hero-head
 			
-		// Hero content: will be in the middle
 		.hero-body
-			.has-text-centered.main-slogan.centered.dots
-				//- h1.title.bolder.prompt.marked-primary liv.tools
-				//- h2.subtitle.lighter by Elya Livshitz
-				div.margin60 &nbsp;
+			h1 Hello
 
-				div
-					.my-logo
-						//- img(src='/resources/imgs/feedox-logo-square.png')
-
-					div.margin20 &nbsp;
-					//- div.block-intro.huge
-					//- 	div.padding.centered
-					//- 		p(style="max-width:350px; line-height:1.5em;").bigger.
-					//- 			The web, for machines. <br/>
-					//- div.margin10 &nbsp;
-
-					.row(layout-gt-xs="row",layout-align="space-between center")
-						div.block-intro.huge
-							//- The web, <br/>for machines. <br/>
-							div.padding.centered.
-								[TBD]&nbsp;
-							div.padding.centered
-								// /Web3.0, <br/>Semantic web.<br/>
-						div &nbsp;
-					
-				//- .btn(@click="$app.helpers.navigate('/profile')").margin20 Create New Card<br/>for Free
-
-
-				//- div
-					.my-logo
-						img(src='/resources/imgs/feedox-logo-square.png')
-
-					div.margin20 &nbsp;
-					//- div.block-intro.huge
-					//- 	div.padding.centered
-					//- 		p(style="max-width:350px; line-height:1.5em;").bigger.
-					//- 			The web, for machines. <br/>
-					//- div.margin10 &nbsp;
-
-					.row(layout-gt-xs="row",layout-align="space-between center")
-						div.block-intro.huge
-							//- The web, <br/>for machines. <br/>
-							div.padding.centered.
-								Coming soon
-							//- div.padding.centered.
-								Web3.0, <br/>Semantic web.<br/>
-						div &nbsp;
-					
-				//- .btn(@click="$app.helpers.navigate('/profile')").margin20 Create New Card<br/>for Free
-
-		// Hero footer: will stick at the bottom
 		.hero-foot
 			
-			//- nav.tabs.is-boxed.is-fullwidth
-				.container
-					ul
-						li.is-active
-							a Overview
-						li
-							a Modifiers
-						li
-							a Grid
-						li
-							a Elements
-						li
-							a Components
-						li
-							a Layout
-											
 </template>
 
 <script lang="ts">
-// import { documentToHtmlString } from '/scripts/ts/browserified/index.js';
 (async()=>{
 	const libs = await System.import('/scripts/ts/browserified/libs.js');
 	console.log('main debug - libs: ', libs, libs.qrcode);
 })()
-// import { libx } from '/frame/scripts/ts/browserified/frame.js';
 import helpers from '/scripts/ts/app/app.helpers.js';
-// import a from '/scripts/ts/browserified/index.js';
-// import x from '/scripts/ts/browserified/libs.js';
-// console.log('main debug: ', a, x);
 
 export default  {
 
@@ -97,9 +24,6 @@ export default  {
 	},
 	created() {
 		helpers.updateMeta({...this.$app.layout.headers, ...{
-			// viewName: 'zuz.to - Digital Business Cards',
-			// pageTitle: 'Web3.0 - Semantic Web',
-			// desc: 'Rediscover the web',
 			desc: 'Innovation first',
 			image: '/resources/imgs/main/main-link-preview-innovation.png',
 		}});
@@ -113,7 +37,6 @@ export default  {
 
 <style lang="less" scoped>
 @import (reference) '../styles/essentials.less';
-// @import (reference) '../styles/style.less';
 @import (reference) '../styles/style.less';
 
 body { background:none; }
